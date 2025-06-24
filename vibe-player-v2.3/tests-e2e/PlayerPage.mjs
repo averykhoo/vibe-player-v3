c91bccb68c1a109be1d74ee47bb1b54d720cc09e
Avery Khoo <averykhoo@gmail.com>
2025-06-24 19:15:05 +0800

    Update PlayerPage.mjs

--- vibe-player-v2.3/tests-e2e/PlayerPage.mjs
+++ vibe-player-v2.3/tests-e2e/PlayerPage.mjs
@@ -17,7 +17,7 @@ export class PlayerPage {
     this.fileInput = page.getByLabel("Load Audio File");
     // ADD THE FOLLOWING TWO LINES:
     this.urlInput = page.getByLabel("Audio URL");
-    this.urlLoadButton = page.getByRole("button", { name: "Load" });
+    this.urlLoadButton = page.getByRole("button", { name: "Load", exact: true });
     this.fileNameDisplay = page.getByTestId("file-name-display");
     this.playButton = page.getByTestId("play-button");
     this.stopButton = page.getByTestId("stop-button");
